d3c4bc160b395925bb1ab360e3a0a00b3b7dba0b
Chaerimy0907 <qkrcofus1026@daum.net>
2025-07-30 14:40:04 +0900

    히스토그램 계산

--- src/line_tracer.py
+++ src/line_tracer.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import matplotlib.pylab as plt
 
 # 웹캠 연결
 cap = cv2.VideoCapture(0)               # 0번 카메라 장치 연결 ---①
@@ -11,6 +13,23 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # height
 if cap.isOpened():                      # 캡쳐 객체 연결 확인
     while True:
         ret, img = cap.read()           # 다음 프레임 읽기
+
+         # 색공간 변환
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # 결과 출력
+        cv2.imshow('Original(BGR)', img)
+        cv2.imshow('HSV', hsv_img)
+        cv2.imshow('Gray', gray_img)
+
+        hist = cv2.calcHist([gray_img], [0], None, [256], [0, 256])
+        plt.plot(hist)
+
+        print("hist.shape : ", hist.shape)
+        print("hist.sum() : ", hist.sum(), "img:shape : ",gray_img.shape)
+        plt.show()
+
         if ret:
             # q 입력 받으면 종료
             key = cv2.waitKey(1) & 0xFF
@@ -20,15 +39,6 @@ if cap.isOpened():                      # 캡쳐 객체 연결 확인
         else:
             print('no frame')
             break
-        
-        # 색공간 변환
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        # 결과 출력
-        cv2.imshow('Original(BGR)', img)
-        cv2.imshow('HSV', hsv_img)
-        cv2.imshow('Gray', gray_img)
 else:
     print("can't open camera.")
 
