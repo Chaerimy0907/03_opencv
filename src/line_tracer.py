70f7eef6572b3089c09ad5320ab14cb55d8c7488
Chaerimy0907 <qkrcofus1026@daum.net>
2025-07-30 14:44:36 +0900

    실행시 영상 멈춤 / q버튼 입력시 종료 안되는 부분 수정

--- src/line_tracer.py
+++ src/line_tracer.py
@@ -9,27 +9,13 @@ cap = cv2.VideoCapture(0)               # 0번 카메라 장치 연결 ---①
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # width
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # height
 
+plt.ion()
+fig, ax = plt.subplots()
 
 if cap.isOpened():                      # 캡쳐 객체 연결 확인
     while True:
         ret, img = cap.read()           # 다음 프레임 읽기
-
-         # 색공간 변환
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        # 결과 출력
-        cv2.imshow('Original(BGR)', img)
-        cv2.imshow('HSV', hsv_img)
-        cv2.imshow('Gray', gray_img)
-
-        hist = cv2.calcHist([gray_img], [0], None, [256], [0, 256])
-        plt.plot(hist)
-
-        print("hist.shape : ", hist.shape)
-        print("hist.sum() : ", hist.sum(), "img:shape : ",gray_img.shape)
-        plt.show()
-
+        
         if ret:
             # q 입력 받으면 종료
             key = cv2.waitKey(1) & 0xFF
@@ -39,6 +25,20 @@ if cap.isOpened():                      # 캡쳐 객체 연결 확인
         else:
             print('no frame')
             break
+        
+        # 색공간 변환
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # 결과 출력
+        cv2.imshow('Original(BGR)', img)
+        cv2.imshow('HSV', hsv_img)
+        cv2.imshow('Gray', gray_img)
+
+        hist = cv2.calcHist([gray_img], [0], None, [256], [0, 256])
+        ax.clear()
+        ax.plot(hist)
+        plt.pause(0.01)
 else:
     print("can't open camera.")
 
